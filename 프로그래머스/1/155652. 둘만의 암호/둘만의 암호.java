319a507c9e91772917fc47339a9eec5a90291757
JiYeon <89963270+hwldus@users.noreply.github.com>
2024-03-11 18:01:29 +0900

    Update 둘만의 암호.java

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -1,7 +1,6 @@
 class Solution {
     public String solution(String s, String skip, int index) {
         String answer = "";
-        StringBuilder sb = new StringBuilder();
         for(int i=0; i<s.length(); i++) {
             char target = s.charAt(i);
             for(int j=0; j<index; j++) {
