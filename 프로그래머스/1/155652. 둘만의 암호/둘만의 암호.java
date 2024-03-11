ce166314857706f7964684942a1009d61e2e1325
JiYeon <89963270+hwldus@users.noreply.github.com>
2024-03-11 17:59:14 +0900

    [level 1] Title: 둘만의 암호, Time: 11.95 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String s, String skip, int index) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<s.length(); i++) {
+            char target = s.charAt(i);
+            for(int j=0; j<index; j++) {
+                target += 1;
+                if(target > 'z')
+                    target -= 26;
+                if(skip.contains(String.valueOf(target)))
+                    j--;
+            }
+            answer += target;
+        }
+        return answer;
+    }
+}
