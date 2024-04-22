db1349685b8234ffccea32a2c7b775aa306a3726
JiYeon <89963270+hwldus@users.noreply.github.com>
2024-04-22 23:44:57 +0900

    [Silver II] Title: 나무 자르기, Time: 516 ms, Memory: 119184 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -2,32 +2,32 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
-        int[] arr = new int[N];
-        int max = 0;
-        for(int i=0; i<N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-            max = Math.max(max, arr[i]);
-        }
-        int left = 0;
-        int right = max;
-        while(left < right) {
-            int mid = (left+right) / 2;
-            long sum = 0;
-            for(int x : arr) {
-                if(x-mid > 0)
-                    sum += x - mid;
-            }
-            if(sum >= M)
-                left = mid + 1;
-            else
-                right = mid;
-        }
-        System.out.println(left-1);
-    }
+   public static void main(String[] args) throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       StringTokenizer st = new StringTokenizer(br.readLine());
+       int n = Integer.parseInt(st.nextToken());
+       int m = Integer.parseInt(st.nextToken());
+       st = new StringTokenizer(br.readLine());
+       int[] arr = new int[n];
+       int high = 0;
+       for(int i=0; i<n; i++) {
+           arr[i] = Integer.parseInt(st.nextToken());
+           high = Math.max(high, arr[i]);
+       }
+       int min = 0;
+       int max = high;
+       while(min < max) {
+           int mid = (min+max) / 2;
+           long sum = 0;
+           for(int x : arr) {
+               if(x-mid > 0)
+                   sum += x-mid;
+           }
+           if(sum >= m)
+               min = mid+1;
+           else
+               max = mid;
+       }
+       System.out.println(min-1);
+   }
 }
