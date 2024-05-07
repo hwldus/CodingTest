1d28c039505d38b92a2f79a468279b193b372806
JiYeon <89963270+hwldus@users.noreply.github.com>
2024-05-07 23:56:35 +0900

    [Silver V] Title: 셀프 넘버, Time: 160 ms, Memory: 14576 KB -BaekjoonHub

---
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[10000];
+        for(int i=0; i<10000; i++) {
+            int num = dx(i+1);
+            if(num <= 10000)
+                arr[num-1] = 1;
+        }
+        for(int i=0; i<10000; i++) {
+            if(arr[i]==0)
+                System.out.println(i+1);
+        }
+    }
+    public static int dx(int n) {
+        int sum = n;
+        while(n != 0) {
+            sum += n%10;
+            n = n/10;
+        }
+        return sum;
+    }
+}
