4b6820770719edda67619e872fe6d717b6b24432
JiYeon <89963270+hwldus@users.noreply.github.com>
2024-06-29 23:25:34 +0900

    [Silver III] Title: N과 M (1), Time: 248 ms, Memory: 23376 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static boolean[] visited;
+    static int[] arr;
+    static int n,m;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        visited = new boolean[n];
+        arr = new int[m];
+        dfs(0);
+        System.out.println(sb);
+    }
+    public static void dfs(int d) {
+        if(d == m) {
+            for(int x : arr) {
+                sb.append(x).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for(int i=0; i<n; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                arr[d] = i+1;
+                dfs(d+1);
+                visited[i] = false;
+            }
+        }
+    }
+}
