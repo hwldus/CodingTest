fbd5fcc6350ab6597a4b61feca1d71a4830c5170
JiYeon <89963270+hwldus@users.noreply.github.com>
2024-11-03 16:19:26 +0900

    [Silver I] Title: 숨바꼭질, Time: 140 ms, Memory: 20768 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,31 +1,31 @@
 import java.io.*;
 import java.util.*;
 
-public class Main { 
-    static int N,K;
+public class Main {
+    static int n,k;
     static int second;
     static boolean[] visited;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        System.out.println(BFS(N,K));
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        int result = bfs(n);
+        System.out.println(result);
     }
-    private static int BFS(int n,int k) {
+    private static int bfs(int node) {
         visited = new boolean[100001];
         Queue<int[]> queue = new ArrayDeque<>();
-        queue.offer(new int[]{n,0});
-        visited[n] = true;
-
+        queue.offer(new int[]{node, 0});
+        visited[node] = true;
         while(!queue.isEmpty()) {
             int[] current = queue.poll();
-            int currentLocation = current[0]; //현재위치
-            second = current[1]; //소요시간
-            if(currentLocation == k) return second;
-
-            int[] nextLocation = {currentLocation-1, currentLocation+1, currentLocation*2};
-            for(int next : nextLocation){
+            int curLocation = current[0];
+            second = current[1];
+            if(curLocation == k) return second;
+            int[] nextLocation = {curLocation-1, curLocation+1, curLocation*2};
+            for(int next : nextLocation) {
                 if(next>=0 && next<=100000 && !visited[next]) {
                     queue.offer(new int[]{next, second+1});
                     visited[next] = true;
