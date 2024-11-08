3a4b80f4f7cc5c8cf43234cb1c0472e93b161b94
JiYeon <89963270+hwldus@users.noreply.github.com>
2024-11-08 17:50:36 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 648 ms, Memory: 142020 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -2,38 +2,40 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N,M, count;
-    static ArrayList<Integer>[] graph;
+    static int N,M,count;
+    static ArrayList<Integer>[] arr;
     static boolean[] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken()); //정점
-        M = Integer.parseInt(st.nextToken()); //간선
-        graph = new ArrayList[N+1];
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new ArrayList[N+1];
         visited = new boolean[N+1];
-        for(int i=1; i<=N; i++)
-            graph[i] = new ArrayList<>();
+        for(int i=1; i<=N; i++) {
+            arr[i] = new ArrayList<>();
+        }
         for(int i=0; i<M; i++) {
-            StringTokenizer str = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(str.nextToken());
-            int end = Integer.parseInt(str.nextToken());
-            graph[start].add(end);
-            graph[end].add(start);
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            arr[start].add(end);
+            arr[end].add(start);
         }
         count = 0;
         for(int i=1; i<=N; i++) {
             if(!visited[i]) {
-                DFS(i);
+                dfs(i);
                 count++;
             }
         }
         System.out.println(count);
     }
-    private static void DFS(int n) {
-        visited[n] = true;
-        for(int next : graph[n]) {
-            if(!visited[next]) DFS(next);
+    private static void dfs(int x) {
+        visited[x] = true;
+        for(int next : arr[x]) {
+            if(!visited[next])
+                dfs(next);
         }
     }
 }
